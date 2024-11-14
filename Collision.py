d7733aa654c79e4416984a6b47d593ff21af89b1
INesso1984 <vanessawithunweb@gmail.com>
2024-11-14 16:43:13 -0600

    Add collision and bullets

--- Collision.py
+++ Collision.py
@@ -7,14 +7,14 @@ pygame.init()
 w_width = 500
 w_height = 500
 screen = pygame.display.set_mode((w_width, w_height))
-screen.fill("red")
-pygame.display.set_caption("Adding enemy")
+screen.fill((255, 0, 0))  # Fill screen with red using RGB values
+pygame.display.set_caption("Collision detection")
 
 clock = pygame.time.Clock()
+
+# Load images for background and character animations
 bg_img = pygame.image.load("images/bg_img.jpeg")
 bg_img = pygame.transform.scale(bg_img, (w_width, w_height))
-
-# Load images for soldier and enemy animations
 walkRight = [pygame.image.load(f'soldier/{i}.png') for i in range(1, 10)]
 walkLeft = [pygame.image.load(f'soldier/L{i}.png') for i in range(1, 10)]
 char = pygame.image.load('soldier/standing.png')
@@ -34,6 +34,7 @@ class Player:
         self.right = False
         self.walkCount = 0
         self.standing = True
+        self.hitbox = (self.x, self.y, self.width, self.height)
 
     def draw(self, screen):
         if self.walkCount + 1 >= 27:
@@ -41,16 +42,19 @@ class Player:
 
         if not self.standing:
             if self.left:
-                screen.blit(walkLeft[self.walkCount // 3], (self.x, self.y))  # Use soldier's walkLeft
+                screen.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
                 self.walkCount += 1
             elif self.right:
-                screen.blit(walkRight[self.walkCount // 3], (self.x, self.y))  # Use soldier's walkRight
+                screen.blit(walkRight[self.walkCount // 3], (self.x, self.y))
                 self.walkCount += 1
         else:
             if self.right:
                 screen.blit(walkRight[0], (self.x, self.y))
             else:
                 screen.blit(walkLeft[0], (self.x, self.y))
+
+        self.hitbox = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(screen, (0, 0, 0), self.hitbox, 2)
 
 class Projectile:
     def __init__(self, x, y, radius, color, direction):
@@ -73,7 +77,7 @@ class Enemy:
         self.walkCount = 0
         self.vel = 3
         self.path = [x, end]
-        self.standing = True
+        self.hitbox = (self.x + 20, self.y, self.width - 40, self.height - 4)
 
     def draw(self, screen):
         if self.walkCount + 1 >= 27:
@@ -83,20 +87,23 @@ class Enemy:
             screen.blit(moveRight[self.walkCount // 3], (self.x, self.y))
         else:
             screen.blit(moveLeft[self.walkCount // 3], (self.x, self.y))
+
         self.walkCount += 1
+        self.hitbox = (self.x + 20, self.y, self.width - 40, self.height - 4)
+        pygame.draw.rect(screen, (0, 0, 0), self.hitbox, 2)
 
     def move(self):
         if self.vel > 0:
             if self.x < self.path[1] - self.width:
                 self.x += self.vel
             else:
-                self.vel = self.vel * -1
+                self.vel = -self.vel
                 self.walkCount = 0
         else:
             if self.x > self.path[0]:
                 self.x += self.vel
             else:
-                self.vel = self.vel * -1
+                self.vel = -self.vel
                 self.walkCount = 0
 
 def DrawInGameloop():
@@ -110,27 +117,35 @@ def DrawInGameloop():
 soldier = Player(50, 435, 64, 64)
 enemy = Enemy(0, 410, 64, 64, w_width)
 bullets = []
-
+shoot = 0
 done = True
+
 while done:
     clock.tick(27)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = False
 
-    for bullet in bullets:
-        if 0 < bullet.x < 500:
-            bullet.x += bullet.vel
-        else:
-            bullets.pop(bullets.index(bullet))
+    if shoot > 0:
+        shoot += 1
+    if shoot > 3:
+        shoot = 0
+
+    for bullet in bullets[:]:
+        if bullet.y - bullet.radius < enemy.hitbox[1] + enemy.hitbox[3] and bullet.y + bullet.radius > enemy.hitbox[1]:
+            if bullet.x + bullet.radius > enemy.hitbox[0] and bullet.x - bullet.radius < enemy.hitbox[0] + enemy.hitbox[2]:
+                bullets.remove(bullet)
+            elif 0 < bullet.x < w_width:
+                bullet.x += bullet.vel
+            else:
+                bullets.remove(bullet)
 
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_SPACE]:
         direction = -1 if soldier.left else 1
         if len(bullets) < 5:
-            bullets.append(Projectile(soldier.x + soldier.width // 2, soldier.y + soldier.height // 2, 6, "#3b3b3b", direction))
+            bullets.append(Projectile(soldier.x + soldier.width // 2, soldier.y + soldier.height // 2, 6, (0, 0, 0), direction))
 
     if keys[pygame.K_LEFT] and soldier.x > 0:
         soldier.x -= soldier.vel
